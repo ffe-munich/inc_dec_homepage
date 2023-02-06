7a75671e5d3f94de595b44da66dd92e36a90d1d1
inc-dec-gaming-dashboards <118459172+inc-dec-gaming-dashboards@users.noreply.github.com>
2023-02-06 15:45:47 +0100

    Add files via upload

--- src/Components/Box/Box_5.jsx
+++ src/Components/Box/Box_5.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import './Box.css'
 
-const Card = ({img1,img2,img3,text_1_1,text_1_2,text_2_1,text_2_2,text_3_1,text_3_2}) => {
+const Card = ({img1,img2,img3,text_1_1,text_1_1_1,text_2_1_1,text_1_2,text_2_1,text_2_2,text_3_1,text_3_2}) => {
   return (
     <div className="card card_5">
 
         <span> Mitigation</span>
         <span> Strategies</span>
         <span>{text_1_1}</span>
+        <span>{text_1_1_1}</span>
         <span>{text_1_2}</span>
-       
-      
         <span>{text_2_1}</span>
+        <span>{text_2_1_1}</span>  
         <span>{text_2_2}</span>
-      
         <span>{text_3_1}</span>
         <span>{text_3_2}</span>
         <span><img src={img1} alt="" style={{height:'30px'}} /></span>
